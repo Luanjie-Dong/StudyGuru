f53fb40b5786fc86a665757b4de86c9b28c2d08d
Vince Tan <vince.tanyy@gmail.com>
2025-03-12 16:17:45 +0800

    fix: idk wat this fixes

--- backend/src/main/java/studyguru/backend/models/Checkpoint.java
+++ backend/src/main/java/studyguru/backend/models/Checkpoint.java
@@ -1,28 +1,21 @@
 package studyguru.backend.models;
 
-// import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
-// @Entity
-// @Table(name = "checkpoint")
 public class Checkpoint {
 
-    // @Id
     private String checkpoint_id;
 
-    // @Column(nullable = false)
     private String checkpoint_name;
 
-    // @Column(nullable = false)
-    private LocalDateTime checkpoint_date;
+    private OffsetDateTime checkpoint_date;
 
-    // @Column(nullable = false)
     private String course_id;
 
     public Checkpoint() {
     }
 
-    public Checkpoint(String checkpoint_id, String checkpoint_name, LocalDateTime checkpoint_date, String course_id) {
+    public Checkpoint(String checkpoint_id, String checkpoint_name, OffsetDateTime checkpoint_date, String course_id) {
         this.checkpoint_id = checkpoint_id;
         this.checkpoint_name = checkpoint_name;
         this.checkpoint_date = checkpoint_date;
@@ -45,11 +38,11 @@ public class Checkpoint {
         this.checkpoint_name = checkpoint_name;
     }
 
-    public LocalDateTime getCheckpoint_date() {
+    public OffsetDateTime getCheckpoint_date() {
         return checkpoint_date;
     }
 
-    public void setCheckpoint_date(LocalDateTime checkpoint_date) {
+    public void setCheckpoint_date(OffsetDateTime checkpoint_date) {
         this.checkpoint_date = checkpoint_date;
     }
 
